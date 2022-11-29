100ec1cf9211b5f76236193ed4718d9481f3d983
Sakar Pudasaini <barcaboysakar@gmail.com>
2022-11-29 17:42:31 +0545

    day 2 commit 2nd

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,29 +1,53 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter your text here");
-  const handleOnChange = (event) => {
+  const [text, setText] = useState('Hello there 🔥');
+  const handleOnChange = event => {
     setText(event.target.value);
   };
   const handleOnClick = () => {
     let newtext = text.toUpperCase();
     setText(newtext);
   };
+  const lowerOnClick = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  };
   return (
-    <div className="container">
-      <div className="mb-3">
-        <h1>{props.heading}</h1>
-        <textarea
-          className="form-control my-3"
-          id="exampleFormControlTextarea1"
-          rows="8"
-          value={text}
-          onChange={handleOnChange}
-        ></textarea>
-        <button className="btn btn-primary" onClick={handleOnClick}>
-          Conver to UpperCase
-        </button>
+    <>
+      <div className="container">
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control my-3"
+            id="exampleFormControlTextarea1"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}></textarea>
+          <button
+            className="btn btn-primary"
+            onClick={handleOnClick}>
+            Conver to UpperCase
+          </button>
+          <button
+            className="btn btn-primary mx-3"
+            onClick={lowerOnClick}>
+            Conver to UpperCase
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="container">
+        <h2>Your text summery</h2>
+        <p>
+          your paragraph have {text.length} letters and {text.split(' ').length-1} words
+        </p>
+        <p>
+          This paragraph takes {text.length*0.008} minutes to read
+        </p>
+
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
